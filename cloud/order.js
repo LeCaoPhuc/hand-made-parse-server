fdef945e3af2d9bc1b50381cb6ebb29ea53b9a30
hoangle <hoangle@Innoria.local>
2017-11-01 15:57:11 +0700

    update order

--- cloud/order.js
+++ cloud/order.js
@@ -27,15 +27,17 @@ Parse.Cloud.define('order', function(req,res) {
     var items = req.params.items;
     var totalPrice = 0;
     var orderNumber = '';
-    var deliveyStatus = 'order';
-    // if(!address || (address && (!address.telephone || !address.firstname || !address.lastname || !address.city))) {
-    //     tools.error(req, res, 'address or any property is undefine', errorConfig.REQUIRE);
-    //     return;
-    // }
-    // if(!items || (items && items.length ==0)){
-    //      tools.error(req, res, 'items empty or undefine', errorConfig.REQUIRE);
-    //     return;
-    // }
+    var deliveryStatus = 'order';
+    var productDetails = [];
+    var orderObject ;
+    if(!address || (address && (!address.telephone || !address.firstname || !address.lastname || !address.city))) {
+        tools.error(req, res, 'address or any property is undefine', errorConfig.REQUIRE);
+        return;
+    }
+    if(!items || (items && items.length ==0)){
+         tools.error(req, res, 'items empty or undefine', errorConfig.REQUIRE);
+        return;
+    }
     var productIdArr = [];
     for(var i= 0; i < items.length; i++) {
         productIdArr.push(items[i].id);
@@ -43,35 +45,78 @@ Parse.Cloud.define('order', function(req,res) {
     var shop = new Parse.Object("Shop");
     shop.id = shopId;
     autoCreateOrderNumber(shop)
-    .then(function(orderNumber){
-        orderNumber = orderNumber;
-        var productQuery = new Parse.Query('ProductDetail');
-        productQuery.containedIn('objectId',productIdArr);
-        productQuery.notContainedIn('status', ['block','delete']);
-        productQuery.include('promotion');
-        productQuery.include('product');
-        return productQuery.find()
+    .then(function(orderNumberString){
+        orderNumber = orderNumberString;
+        var productDetailQuery = new Parse.Query('ProductDetail');
+        productDetailQuery.containedIn('objectId',productIdArr);
+        productDetailQuery.notContainedIn('status', ['block','delete']);
+        productDetailQuery.include('promotion');
+        productDetailQuery.include('product');
+        return productDetailQuery.find()
     })
     .then(function(results){
         console.log(results);
         if(results && results.length > 0) {
+            productDetails = results;
             var Order = Parse.Object.extend("Order");
             var order = new Order();
             order.set('shop', shop);
             order.set('buyer',user);
-            var arrOrderDetailQuery = [];
+            order.set('delivery_address', address);
+            order.set('delivery_status', deliveryStatus);
+            order.set('order_number', orderNumber);
             for(var i = 0 ; i < results.length; i++) {
-                var OrderDetail = Parse.Object.extend("OrderDetail");
-                var orderDetail = new OrderDetail();
-                orderDetail
+                if(results[i].get('promotion')) {
+                    var price = results[i].get('promotion').get('percent')*results[i].get('price');
+                    totalPrice += price;
+                }
+                else {
+                    totalPrice += results[i].get('price');
+                }
             }
+            order.set('total_price',totalPrice);
+            return order.save(null,{ useMasterKey: true })
         }
         else {
             tools.error(req,res, 'product not found', errorConfig.NOT_FOUND);
         }
     })
+    .then(function(order){
+        console.log(address.city);
+        orderObject = order;
+        var arrPromiseSaveOrderDetail = [];
+        for(var i = 0 ; i < productDetails.length; i++) {
+            for(var j = 0; j < items.length ; j++) {
+                if(productDetails[i].id == items[j].id) {
+                    var OrderDetail = Parse.Object.extend("OrderDetail");
+                    var orderDetail = new OrderDetail();
+                    orderDetail.set('quantity_buy', items[j].qty);
+                    orderDetail.set('product_detail',productDetails[i]);
+                    orderDetail.set('order',order);
+                    if(productDetails[i].get('promotion')) {
+                        var price = productDetails[i].get('promotion').get('percent')*productDetails[i].get('price');
+                        orderDetail.set('unit_price', price);
+                        orderDetail.set('total_price_product', price * items[j].qty)
+                    }
+                    else {
+                        orderDetail.set('unit_price', productDetails[i].get('price'));
+                        orderDetail.set('total_price_product', productDetails[i].get('price') * items[j].qty)
+                    }
+                    arrPromiseSaveOrderDetail.push(orderDetail.save(null,{useMasterKey : true }));
+                }
+            }
+        }
+        return Promise.all(arrPromiseSaveOrderDetail);
+    })
+    .then(function(arrOrderDetail){
+        var response = {
+            orderInfo : orderObject,
+            items: arrOrderDetail
+        };
+        tools.success(req, res, 'order success', response);
+    })
     .catch(function(err){
-        console.log(err);
+       tools.error(req, res, 'order fail in catch',errorConfig.ACTION_FAIL, err);
     })
 })
 
