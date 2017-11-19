4032d4ecb2547a65f500e1d06aa285d85fea457e
LeCaoPhuc <lecaobaophuc@gmail.com>
2017-11-19 22:46:25 +0700

    change config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var env = process.env.ENV || 'dev'
 , path = require('path')
 , configs = {};
-var url = 'http://192.168.1.158:';
+var url = 'http://192.168.43.231:';
 configs.dev = {
     
     port: 3000,
